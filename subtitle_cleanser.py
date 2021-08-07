fddbdeec7a4d5433f8216bb3bffba06ae23dd64f
Jace Tan <wjacetan@gmail.com>
2021-08-07 16:49:12 +0800

    Add filename and try/except block to ensure subsFile can be opened and read

--- subtitle_cleanser.py
+++ subtitle_cleanser.py
@@ -3,6 +3,15 @@
 def main():
     # Open the subtitle file
     # Make sure the subtitle file is in the same level as this file
+    filename = "24 Hurt Me, Hurt You.srt"
+
+    # Check that subsFile can be read
+    try:
+        subsFile = open(filename, "r")
+    except FileNotFoundError:
+        print("Unable to find or open the file: {}.".format(filename))
+        return
+
     # Read enough lines for the first block
     # Determine which lines are content lines (could be 1, 2 or even 3 lines)
     # Run the content lines through multiple stages of cleansing
