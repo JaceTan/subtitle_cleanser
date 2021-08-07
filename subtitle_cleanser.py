e2d56c77e169661cf71bb967c38d3ca6cd68c766
Jace Tan <wjacetan@gmail.com>
2021-08-07 16:47:56 +0800

    Move pseudo code into a main func

--- subtitle_cleanser.py
+++ subtitle_cleanser.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
-# Open the subtitle file
-# Read lines enough for the first block
-# Determine which lines are content lines (could be 1, 2 or even 3 lines)
-# Run the content lines through multiple stages of cleansing
-# Determine if there's any content lines left
-# If yes, write to a new file
-# If no, skip the whole block
+def main():
+    # Open the subtitle file
+    # Make sure the subtitle file is in the same level as this file
+    # Read enough lines for the first block
+    # Determine which lines are content lines (could be 1, 2 or even 3 lines)
+    # Run the content lines through multiple stages of cleansing
+    # Determine if there's any content lines left
+    # If yes, write to a new file
+    # If no, skip the whole block
+
+main()
