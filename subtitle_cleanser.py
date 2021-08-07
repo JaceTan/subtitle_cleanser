fa467d674216d3c2007d4037ec0ef9ff80f4fa90
Jace Tan <wjacetan@gmail.com>
2021-08-07 17:06:48 +0800

    Add getNextSubtitleBlock func with docstring

--- subtitle_cleanser.py
+++ subtitle_cleanser.py
@@ -1,5 +1,32 @@
 #!/usr/bin/env python3
 
+def getNextSubtitleBlock(subsFile):
+    """
+    Reads the few next lines of the subtitle file to
+    identify, create and return a subtitleBlock dictionary.
+
+    A subtitle block should have the following pattern:
+    Section Index Number
+    Timestamp --> Timestamp
+    Content Line1
+    Content Line2 or Newline
+    Content Line3 or Newline
+
+    Example:
+    1
+    00:00:00,045 --> 00:00:01,361
+    WATSON:
+    <i>Previously on</i> Elementary...
+
+    Parameters
+    ----------
+    subsFile: File. The file object to read subtitle lines from.
+
+    Returns
+    -------
+    Dictionary. The next subtitleBlock in distinct components.
+    """
+
 def main():
     # Open the subtitle file
     # Make sure the subtitle file is in the same level as this file
@@ -13,6 +40,8 @@ def main():
         return
 
     # Read enough lines for the first block
+    subtitleBlock = getNextSubtitleBlock(subsFile)
+
     # Determine which lines are content lines (could be 1, 2 or even 3 lines)
     # Run the content lines through multiple stages of cleansing
     # Determine if there's any content lines left
